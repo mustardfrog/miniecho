284fc6ff02ad5a99d387b0a6bbe4ccc77a9c84a9
Frog <frogdoctor@tutanota.com>
2023-05-02 15:01:27 +0800

    add new line if no args are provided

--- src/main.rs
+++ src/main.rs
@@ -6,15 +6,11 @@ fn main() {
 
 fn echo() {
     let args: Vec<_> = env::args().collect();
-
-    if args.len() <= 1 {
-        println!("");
-    }
     let mut result = String::new();
+
     for i in args.iter().skip(1) {
         result.push_str(i);
         result.push_str(" ");
     }
     println!("{result}");
-    
 }
